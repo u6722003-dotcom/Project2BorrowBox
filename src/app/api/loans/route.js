6af6a71411c9906fe3c8dbbcf36199a3ea32777b
Andrei Filip <u6722003@au.edu>
2026-02-26 20:52:53 +0700

    Fix mongodb import for production

--- src/app/api/loans/route.js
+++ src/app/api/loans/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/mongodb";
+import { dbConnect } from "@/lib/mongodb";
 import Loan from "@/models/Loan";
 
 // Handle creating a new Borrow Record (Loan)
